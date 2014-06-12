59d96bb5c0c254871e2600ce499e7c6c06c87a92
vinayganeshh <vinayganeshh@gmail.com>
2014-06-11 22:09:35 -0700

    Initializing the application

---
+++ WriteToText/MainPage.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using WriteToText.Resources;
+using Windows.Storage;
+using System.Diagnostics;
+
+namespace WriteToText
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        StorageFolder folder;
+        StorageFile TimeLogFile;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected async override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            folder = KnownFolders.PicturesLibrary;
+            TimeLogFile = await folder.CreateFileAsync("TimeLog.txt", CreationCollisionOption.OpenIfExists);
+        }
+
+        public async void WriteToLog()
+        {
+            await Windows.Storage.FileIO.WriteTextAsync(TimeLogFile, "Start Logging " + DateTime.Now.ToString() + Environment.NewLine);
+
+            string[] a = { "windows", "phone", "8.1", "owns", "all" };
+
+            for(int i = 0; i < a.Length; i ++)
+            {
+                await Windows.Storage.FileIO.AppendTextAsync(TimeLogFile, a[i] + " - " + DateTime.Now.ToString() + Environment.NewLine);
+            }
+
+            await Windows.Storage.FileIO.AppendTextAsync(TimeLogFile, "End Logging " + DateTime.Now.ToString() + Environment.NewLine);
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            WriteToLog();
+        }
+
+       public async void appendToLog(string text)
+        {
+            await Windows.Storage.FileIO.AppendTextAsync(TimeLogFile, text +" - " + DateTime.Now.ToString() + Environment.NewLine);
+        }
+
+       public async void writeToLog(string text)
+       {
+           await Windows.Storage.FileIO.WriteTextAsync(TimeLogFile, text + " - " + DateTime.Now.ToString() + Environment.NewLine);
+       }
+
+       private void btnLogException_Click(object sender, RoutedEventArgs e)
+       {
+           try
+           {
+               int a = 10;
+               int b = 0;
+
+               int c = a / b;
+           }
+           catch (Exception ex)
+           {
+               appendToLog(ex.Message);
+           }
+           
+
+       }
+    }
+}
